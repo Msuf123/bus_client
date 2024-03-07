4cd5c0a6ec8dc7bf924f97c75ea4bfd582d522be
Msuf123 <akshatmalik18t@gmail.com>
2024-03-07 08:37:59 +0530

    workig

--- src/Components/SeatBook/IndividualSeat/IndividualSeat.js
+++ src/Components/SeatBook/IndividualSeat/IndividualSeat.js
@@ -1,8 +1,18 @@
+
+import { useState } from 'react'
+import SeatHoverBanner from '../SeatHoverBanner/SeatHoverBanner'
 import style from './individual.module.css'
 export default function IndividualSeat(){
+    const [visibility,setVisibility]=useState(false)
     return(
-        <div className={style.main}>
-            
+        <div className={style.main} onMouseEnter={()=>{
+            setVisibility(true)
+        }}
+        onMouseLeave={()=>{
+            setVisibility(false)
+        }}
+        >
+           {visibility ?<SeatHoverBanner></SeatHoverBanner>:null}
         </div>
     )
 }
---
+++ src/Components/SeatBook/SeatHoverBanner/SeatHoverBanner.js
@@ -0,0 +1,8 @@
+import style from './banner.module.css'
+export default function SeatHoverBanner(){
+    return(
+        <div className={style.main}>
+          <span>&#8377;99</span>
+        </div>
+    )
+}
